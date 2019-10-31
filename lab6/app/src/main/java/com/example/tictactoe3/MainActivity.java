b6963e5dca067d7e34b62a1856379505f07dda87
AEKurekci <aliemrekurekci@gmail.com>
2019-10-31 15:11:55 +0300

    lab7: added saveGame and loadGame menu items added

--- lab6/app/src/main/java/com/example/tictactoe3/MainActivity.java
+++ lab6/app/src/main/java/com/example/tictactoe3/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.tictactoe3;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -198,6 +200,51 @@ public class MainActivity extends AppCompatActivity {
             for(int j = 0; j < 3;j++) {
                 Button btn = (Button) row.getChildAt(j);
                 btn.setText("");
+            }
+        }
+        return true;
+    }
+
+    public boolean saveGame(MenuItem item){
+
+        SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferences.edit();
+
+        long b = 0;
+        for (int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                b += (long)(board[i][j] * Math.pow(10, (i * 3) + j));
+            }
+        }
+        editor.putLong("board", b);
+        editor.putBoolean("turn", player1Turn);
+        editor.commit();
+        return true;
+    }
+
+    public boolean loadGame(MenuItem item){
+        SharedPreferences preferences = getPreferences(Context.MODE_PRIVATE);
+        player1Turn = preferences.getBoolean("turn", true);
+        long b = preferences.getLong("board",0);
+        for (int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                board[i][j] = (byte)((b/Math.pow(10, (i * 3) + j)) % 10);
+            }
+        }
+        //update button labels
+        for (int i = 0;i < 3;i++) {
+            TableLayout table = findViewById(R.id.table);
+            TableRow row = (TableRow) table.getChildAt(i);
+            for(int j = 0; j < 3;j++) {
+                Button btn = (Button) row.getChildAt(j);
+                switch (board[i][j]){
+                    case 0: btn.setText("");
+                        break;
+                    case 1: btn.setText(PLAYER_1_SYMBOL);
+                        break;
+                    case 2: btn.setText(PLAYER_2_SYMBOL);
+                }
+
             }
         }
         return true;
